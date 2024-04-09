fe74a84561a4c8bb65cad183f17c8150ae96738c
Bobby <s215818@dtu.dk>
2024-04-09 14:50:01 +0200

    Isyourhorseshoeontheotherhoof228A.cpp

---
+++ Problemsets/Isyourhorseshoeontheotherhoof228A.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<long long>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using vii = std::vector<pii>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+}
+
+int main() {
+    FAST_IO;
+
+    vi shoes;
+    int shoe;
+    int shoe_needed = 0;
+
+    for (int i = 0; i < 4; ++i) {
+        std::cin >> shoe;
+        shoes.push_back(shoe);
+    }
+
+    for (int i = 0; i < shoes.size(); ++i) {
+        for (int j = i + 1; j < shoes.size(); ++j) {
+            if (shoes[i] == shoes[j]) {
+                ++shoe_needed;
+                break;
+            }
+        }
+
+    }
+    std::cout << shoe_needed << '\n';
+
+    return 0;
+}
