bbbd1d76ae6f755c2a606de89ce77f7ac97cc84a
Bobby <s215818@dtu.dk>
2024-04-12 02:50:52 +0200

    Puzzles337A.cpp

---
+++ Problemsets/900Rating/Puzzles337A.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<long long>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using vii = std::vector<pii>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+}
+
+int main() {
+    FAST_IO;
+
+    int n, k;
+    std::cin >> n >> k;
+
+    vi a(k);
+    read_vector(a);
+
+    std::sort(a.begin(), a.end());
+
+    int min_diff = 100000;
+    for (int i = 0; i < k-n+1; ++i) {
+        min_diff = std::min(min_diff, a[i + n - 1] - a[i]);
+    }
+    std::cout << min_diff << '\n';
+
+    return 0;
+}
