d5c6a32e8e6ace0c557ce7bb67648611fd60aae9
Bobby <s215818@dtu.dk>
2024-04-14 06:15:57 +0200

    LuckyNumbers1808A.cpp

---
+++ Problemsets/900Rating/LuckyNumbers1808A.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<long long>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using vii = std::vector<pii>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        ll l, r;
+        std::cin >> l >> r;
+        int max = -9999999, ans;
+
+        for (ll i = l; i <= r; i++) {
+            std::string s = std::to_string(i);
+            sort(s.begin(), s.end());
+            int ff = s[0] - '0';
+            int ll = s[s.size() - 1] - '0';
+            int res = ll-ff;
+
+            if (res > max) {
+                max = res;
+                ans = i;
+            }
+
+            if (max == 9) break;
+        }
+        std::cout << ans << '\n';
+    }
+
+    return 0;
+}
