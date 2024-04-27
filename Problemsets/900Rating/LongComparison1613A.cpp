9106bb76333be7d8d9bfb976ce97f1fdb676e155
Bobby <s215818@dtu.dk>
2024-04-27 03:21:30 +0200

    LongComparison1613A.cpp

---
+++ Problemsets/900Rating/LongComparison1613A.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using vii = std::vector<pii>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+}
+
+void solve() {
+    ll x1, p1, x2, p2;
+    std::cin >> x1 >> p1 >> x2 >> p2;
+    int min = std::min(p1,p2);
+    p1-=min;
+    p2-=min;
+    if (p1>=7) std::cout << ">" << '\n';
+    else if (p2>=7) std::cout << "<" << '\n';
+    else {
+        for (int i = 0; i < p1; ++i) x1 *= 10;
+        for (int i = 0; i < p2; ++i) x2 *= 10;
+        if (x1 == x2) std::cout << "=" << '\n';
+        else if (x1 > x2) std::cout << ">" << '\n';
+        else std::cout << "<" << '\n';
+    }
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
