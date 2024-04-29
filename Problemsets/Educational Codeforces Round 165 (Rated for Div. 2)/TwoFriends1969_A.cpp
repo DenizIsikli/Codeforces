d2ce523a54794cd114c3f0e31518735d366d08a3
Bobby <s215818@dtu.dk>
2024-04-29 18:42:05 +0200

    Educational Codeforces Round 165 (Rated for Div. 2)

---
+++ Problemsets/Educational Codeforces Round 165 (Rated for Div. 2)/TwoFriends1969_A.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using vii = std::vector<pii>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+}
+
+void solve() {
+    int MAXN = 5e5+7;
+    int n;
+    std::cin >> n;
+    int a[MAXN];
+    for (int i = 1; i <= n; ++i) std::cin >> a[i];
+    int res = 4;
+    for (int i = 1; i <= n; ++i) {
+        std::set<int> s;
+        s.insert(i);
+        s.insert(a[i]);
+        s.insert(a[a[i]]);
+        res = std::min(res, (int)s.size());
+    }
+    std::cout << res << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
