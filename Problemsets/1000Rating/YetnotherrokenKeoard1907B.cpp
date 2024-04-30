9e837a3b4f1150060189fd7d441655da93ebbdff
Bobby <s215818@dtu.dk>
2024-04-30 05:28:23 +0200

    YetnotherrokenKeoard1907B.cpp

---
+++ Problemsets/1000Rating/YetnotherrokenKeoard1907B.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using vii = std::vector<pii>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+}
+
+void solve() {
+    std::string str;
+    std::cin >> str;
+    std::string ans;
+    int lower = 0, upper = 0;
+
+    for (int i=(int)(str).size()-1; i>=0; --i) {
+        char c = str[i];
+        if (islower(c)) {
+            if (c=='b') lower++;
+            else if (lower) lower--;
+            else ans += c;
+        } else {
+            if (c == 'B') upper++;
+            else if (upper) upper--;
+            else ans += c;
+        }
+    }
+    std::reverse(ans.begin(), ans.end());
+    std::cout << ans << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
