2a731b39c4d7ec1c28bd052396e00906ae44ac23
Bobby <s215818@dtu.dk>
2024-05-04 23:55:56 +0200

    OlyaandGamewithArrays1859.cpp

---
+++ Problemsets/1000Rating/OlyaandGamewithArrays1859.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using vii = std::vector<pii>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+}
+
+void solve() {
+    int n;
+    std::cin >> n;
+    int min_v = INF;
+    vi min_vs;
+    for (int i = 0; i < n; ++i) {
+        int m;
+        std::cin >> m;
+        vi v(m);
+        read_vector(v);
+
+        int min_val = *std::min_element(v.begin(), v.end());
+        min_v = std::min(min_v, min_val);
+        v.erase(std::find(v.begin(), v.end(), min_val));
+        min_vs.push_back(*std::min_element(v.begin(), v.end()));
+    }
+    std::cout << min_v + (ll)std::accumulate(min_vs.begin(), min_vs.end(), 0LL) - *std::min_element(min_vs.begin(), min_vs.end()) << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
