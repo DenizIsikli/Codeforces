fe2d5c6f35ad2d62fbd3aa759f20e883fde4c124
Deniz <s215818@dtu.dk>
2024-06-23 22:28:19 +0200

    UpdateQueries1986_C.cpp

---
+++ Problemsets/Codeforces Round 954 (Div. 3)/UpdateQueries1986_C.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using pll = std::pair<ll, ll>;
+using vii = std::vector<pii>;
+using vll = std::vector<pll>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+    std::cout << '\n';
+}
+
+void solve() {
+    int n, m; cin >> n >> m;
+    string s; cin >> s;
+
+    vi idcs(m); read_vector(idcs);
+    string q; cin >> q;
+
+    set<int> uni_idcs(idcs.begin(), idcs.end());
+    vi srt_idcs(uni_idcs.begin(), uni_idcs.end());
+    sort(srt_idcs.begin(), srt_idcs.end());
+
+    sort(q.begin(), q.end());
+
+    map<int, char> idx_to_char;
+    for (size_t i = 0; i < srt_idcs.size(); ++i) idx_to_char[srt_idcs[i]] = q[i];
+    for (const auto [idx, ch] : idx_to_char) s[idx-1] = ch;
+
+    cout << s << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
