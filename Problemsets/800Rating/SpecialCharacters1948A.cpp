db1971205bbdff42a99e00d1c19478e4b9c19256
Deniz <s215818@dtu.dk>
2024-06-25 23:57:35 +0200

    SpecialCharacters1948A.cpp

---
+++ Problemsets/800Rating/SpecialCharacters1948A.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using vii = std::vector<pii>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n%2==1) {
+            cout << "NO" << '\n';
+            continue;
+        }
+        cout << "YES" << '\n';
+        for (int i = 0; i < n/2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                cout << "AB"[i&1];
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
+
+
+
+
