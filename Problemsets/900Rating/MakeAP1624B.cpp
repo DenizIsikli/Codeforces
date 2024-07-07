856af31d4259f0ff3324a860664c1b26771cf692
Deniz <s215818@dtu.dk>
2024-07-07 19:12:55 +0200

    MakeAP1624B.cpp

--- Problemsets/900Rating/MakeAP1624B.cpp
+++ Problemsets/900Rating/MakeAP1624B.cpp
@@ -65,7 +65,7 @@ void solve() {
     if ((2*b-c)>0 && (2*b-c)%a==0) cout << "YES" << '\n';
     else if ((a+c)%(2*b)==0) cout << "YES" << '\n';
     else if ((2*b-a)>0 && (2*b-a)%c==0) cout << "YES" << '\n';
-    else cout << "NO" << '\n';   
+    else cout << "NO" << '\n';  
 }
 
 int main() {
