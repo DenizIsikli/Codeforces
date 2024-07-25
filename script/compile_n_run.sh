3e3ab7f00fb6e1365a25d1b12b479d8876b186b6
Deniz Isikli <s215818@dtu.dk>
2024-07-26 00:04:46 +0200

    Modified compile script to save in specified folder

--- script/compile_n_run.sh
+++ script/compile_n_run.sh
@@ -6,17 +6,31 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+# Get the script directory (where this script is located)
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+# Define the 'executables' directory path within the script directory
+executables_dir="$script_dir/executables"
+
+# Create the 'executables' directory if it doesn't exist
+mkdir -p "$executables_dir"
+
 # Get the file name without extension
 filename=$(basename -- "$1")
 filename_without_extension="${filename%.*}"
 
+# Define the output executable path
+output_executable="$executables_dir/$filename_without_extension"
+
 # Compile the C++ file with the desired C++ standard
-g++ -std=c++20 -o "$filename_without_extension" "$1" template.h
+g++ -std=c++20 -o "$output_executable" "$1" template.h
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
-  echo "Compilation successful. Running the program..."
-  ./"$filename_without_extension"
+  echo "Compilation successful"
+  echo "Executable saved to $output_executable"
+  echo "Running the program"
+  "$output_executable"
 else
-  echo "Compilation failed."
+  echo "Compilation failed"
 fi
