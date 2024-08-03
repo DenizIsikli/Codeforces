f5f1cd7dcf2a1c32719a6a41d20f3465e97e3ecc
Deniz Isikli <s215818@dtu.dk>
2024-08-03 23:51:09 +0200

    SettingupCamp1945A.cpp

--- Problemsets/800Rating/SettingupCamp1945A.cpp
+++ Problemsets/800Rating/SettingupCamp1945A.cpp
@@ -60,7 +60,19 @@ void print_vector_ws(const std::vector<T> &vec) {
 }
 
 void solve() {
+    ll single, poly, uni;
+    cin >> single >> poly >> uni;
+    ll needPoly = (3 - poly % 3) % 3;
+    if (poly > 0 && needPoly > uni) {
+        cout << "-1\n";
+        return;
+    }
 
+    uni -= needPoly;
+    poly += needPoly;
+
+    ll mn = single + uni / 3 + (uni % 3 + 1) / 2 + poly / 3;
+    cout << mn << '\n';
 }
 
 int main() {
