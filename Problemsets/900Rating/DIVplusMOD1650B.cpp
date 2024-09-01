c3e6a725a90df16fc0bc06729a78ddd905c1b2a7
Deniz Isikli <s215818@dtu.dk>
2024-09-01 23:16:17 +0200

    DIVplusMOD1650B.cpp

--- Problemsets/900Rating/DIVplusMOD1650B.cpp
+++ Problemsets/900Rating/DIVplusMOD1650B.cpp
@@ -62,7 +62,12 @@ void print_vector_ws(const std::vector<T> &vec) {
 }
 
 void solve() {
-
+    int l, r, x;
+    cin >> l >> r >> x;
+    int ans = r/x+r%x;
+    int m = r/x*x-1;
+    if (m >= l) ans = max(ans, m/x+m%x);
+    cout << ans << '\n';
 }
 
 int main() {
