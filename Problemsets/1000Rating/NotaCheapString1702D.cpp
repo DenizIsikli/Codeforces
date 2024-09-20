abe657cf6bce0e27a2fcab6e990b88d9e0cc57df
Deniz Isikli <s215818@dtu.dk>
2024-09-20 23:44:43 +0200

    NotaCheapString1702D.cpp

---
+++ Problemsets/1000Rating/NotaCheapString1702D.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using pll = std::pair<ll, ll>;
+using vii = std::vector<pii>;
+using vll = std::vector<pll>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+    std::cout << '\n';
+}
+
+void solve() {
+    string s; int p;
+    cin >> s >> p;
+
+    string w = s;
+    string res;
+    sort(w.rbegin(), w.rend());
+    int cost = 0;
+    for (int i = 0; i < w.size(); i++) {
+        cost += s[i]-'a'+1;
+    }
+    map<char, int> del;
+    for (int i = 0; i < w.size(); i++) {
+        if (cost > p)  {
+            del[w[i]]++;
+            cost -= w[i]-'a'+1;
+        }
+    }
+
+    for (int i = 0; i < s.size(); i++) {
+        if (del[s[i]] > 0) {
+            del[s[i]]--;
+            continue;
+        }
+        cout << s[i];
+    }
+    cout << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
