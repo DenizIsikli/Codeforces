378766aaead8c83bb5acf3e191eecbc1dec1dd80
Deniz Isikli <s215818@dtu.dk>
2024-09-26 23:10:18 +0200

    RightLeftWrong200D.cpp

---
+++ Problemsets/1200Rating/RightLeftWrong2000D.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using pll = std::pair<ll, ll>;
+using vii = std::vector<pii>;
+using vll = std::vector<pll>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+    std::cout << '\n';
+}
+
+void solve() {
+    ll n; cin >> n;
+    vl a(n+1);
+    vl prefix(n+1);
+    prefix[0] = 0;
+    for (ll i = 1; i <= n; i++) {
+        cin >> a[i];
+        prefix[i] = prefix[i-1] + a[i];
+    }
+
+    string s; cin >> s; s = ' '+s;
+    ll ans = 0, l = 1, r = n;
+
+    while (l<r) {
+        while (l<n && s[l]=='R') l++;
+        while (r >= 0 && s[r]=='L') r--;
+        if (l<r) {
+            ans+=prefix[r]-prefix[l-1];
+            l++, r--;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
