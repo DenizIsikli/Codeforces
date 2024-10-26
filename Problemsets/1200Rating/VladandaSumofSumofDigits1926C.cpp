71031cf5cb62e4d4d1540502883cc332a0b17678
Deniz Isikli <s215818@dtu.dk>
2024-10-26 22:52:47 +0200

    VladandaSumofSumofDigits1926C.cpp

---
+++ Problemsets/1200Rating/VladandaSumofSumofDigits1926C.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+// Common data types
+using ll = long long;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vs = std::vector<std::string>;
+using pii = std::pair<int, int>;
+using pll = std::pair<ll, ll>;
+using vii = std::vector<pii>;
+using vll = std::vector<pll>;
+using si = std::set<int>;
+using mii = std::map<int, int>;
+using umii = std::unordered_map<int, int>;
+using usi = std::unordered_set<int>;
+
+// Constants
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+template<typename T>
+void print_vector(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val << ' ';
+    std::cout << '\n';
+}
+
+template<typename T>
+void print_vector_ws(const std::vector<T> &vec) {
+    for (const T &val : vec) std::cout << val;
+    std::cout << '\n';
+}
+
+const int MAX = 200007;
+int res[MAX];
+
+void solve() {
+    ll n; cin >> n;
+    cout << res[n] << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    auto sum_of_digits = [](ll x) {
+        ll sum = 0;
+        while (x) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    };
+
+    for (int i = 1; i < MAX; ++i) {
+        res[i] = res[i-1]+sum_of_digits(i);
+    }
+    
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
