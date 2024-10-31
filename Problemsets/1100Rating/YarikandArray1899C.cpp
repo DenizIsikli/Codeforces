4c4ed59de542fb1c76f8ab344ec16a7fc907e256
Deniz Isikli <s215818@dtu.dk>
2024-10-31 22:58:39 +0100

    YarikandArray1899C.cpp

---
+++ Problemsets/1100Rating/YarikandArray1899C.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+using ll = long long;
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    int n; cin >> n;
+    int a[2000010];
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+
+    ll res = MIN_INT, sum = 0;
+
+    for(int i=1;i<=n;i++){
+        if(i>1&&(a[i]+a[i-1])%2==0) sum=0;
+        sum+=a[i];
+        res=max(res, sum);
+        if(sum<0) sum=0;
+    }
+    cout << res << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
