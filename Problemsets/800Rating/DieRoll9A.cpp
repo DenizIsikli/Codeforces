34b8b7eea36cb612e167c8b81cbaf350bff5bd4d
Deniz Isikli <s215818@dtu.dk>
2024-11-09 23:19:17 +0100

    DieRoll9A.cpp

---
+++ Problemsets/800Rating/DieRoll9A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+using ll = long long;
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+int main() {
+    FAST_IO;
+
+    int a, b; cin >> a >> b;
+    int f = 7-max(a,b);
+    int g = __gcd(f,6);
+    cout << f/g << "/" << 6/g << '\n';
+
+    return 0;
+}
+
