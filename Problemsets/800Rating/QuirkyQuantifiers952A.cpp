3a14ba8f52f6bbab5eaf4e798471ded0a0f8d484
Deniz Isikli <s215818@dtu.dk>
2024-11-16 23:36:36 +0100

    QuirkyQuantifiers952A.cpp

---
+++ Problemsets/800Rating/QuirkyQuantifiers952A.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+using ll = long long;
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    int n; cin >> n;
+    cout << n%2 << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    solve();
+
+    return 0;
+}
+
+
