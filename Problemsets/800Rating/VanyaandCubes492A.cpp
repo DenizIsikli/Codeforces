ff7cf0893b2b5be94de46b5a477e9bf8f846854f
Deniz Isikli <s215818@dtu.dk>
2024-11-17 23:30:35 +0100

    VanyaandCubes492A.cpp

---
+++ Problemsets/800Rating/VanyaandCubes492A.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+using ll = long long;
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    int n; cin >> n;
+    int i = 1;
+    int sum = 0;
+    int ans = 0;
+    while (sum <= n) {
+        sum += i * (i + 1) / 2;
+        if (sum <= n) {
+            ++ans;
+        }
+        ++i;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    solve();
+
+    return 0;
+}
+
+
