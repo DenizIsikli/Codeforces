3f2dcc68c44b7330ed226c077b01782330cfdbe1
Deniz Isikli <s215818@dtu.dk>
2024-11-21 22:45:17 +0100

    QuestionMarks1993A.cpp

---
+++ Problemsets/800Rating/QuestionMarks1993A.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+using ll = long long;
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+
+    if (all_of(s.begin(), s.end(), [](char c) { return c == '?'; })) {
+        cout << 0 << '\n';
+        return;
+    }
+
+    map<char, int> fq;
+    for (char &c : s) {
+        fq[c]++;
+    }
+    
+    int res = 0;
+    for (auto &[k, v] : fq) {
+        if (k=='?') continue;
+        res += min(n, v);
+    }
+    cout << res << '\n';
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
