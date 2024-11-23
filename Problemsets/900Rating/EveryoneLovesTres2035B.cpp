2b106ebdbecf09dc3936d685d7514ad28eb10ab0
Deniz Isikli <s215818@dtu.dk>
2024-11-23 23:51:12 +0100

    EveryoneLovesTres2035B.cpp

---
+++ Problemsets/900Rating/EveryoneLovesTres2035B.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros and optimizations
+#pragma GCC optimize ("O3")
+#define FAST_IO std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+using ll = long long;
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+// Template functions
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    int n; cin >> n;
+    if (n==1 || n==3) {
+        cout << -1 << '\n';
+        return;
+    } else if (n%2==0) {
+        for (int i = 0; i < n-2; i++) {
+            cout << "3";
+        }
+        cout << "66\n";
+    } else {
+        for (int i = 0; i < n-5; i++) {
+            cout << "3";
+        }
+        cout << "36366\n";
+    }
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
