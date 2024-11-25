19402a9a93fec1d2b72841054f99dc86bb5bdeed
Deniz Isikli <s215818@dtu.dk>
2024-11-25 23:17:31 +0100

    BestBinaryString183C.cpp

---
+++ Problemsets/1000Rating/BestBinaryString1837C.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    string s; cin >> s;
+    char x = '0';
+    for (auto &c : s) {
+        if (c == '?') c = x;
+        x = c;
+    }
+    cout << s << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
