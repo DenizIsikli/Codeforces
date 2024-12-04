60ee178fb442730293ab19d925afb0ed323a1ed8
Deniz Isikli <s215818@dtu.dk>
2024-12-04 22:08:32 +0100

    NastiaandNearlyGoodNumbers.cpp

---
+++ Problemsets/1000Rating/NastiaandNearlyGoodNumbers.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    if (b==1) {
+        cout << "NO" << '\n';
+        return;
+    } else {
+        cout << "YES" << '\n';
+        cout << a << ' ' << a*(long long)b << ' ' << a*(long long) (b+1) << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
