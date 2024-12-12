5e73568391dcbd3e18612a21e97047148fd7d9ee
Deniz Isikli <s215818@dtu.dk>
2024-12-12 23:38:46 +0100

    AGiftFromOrangutan2030A.cpp

---
+++ Problemsets/800Rating/AGiftFromOrangutan2030A.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+using vi = std::vector<int>;
+
+#define int long long
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    int mx = MIN_INT;
+    int min = MAX_INT;
+
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        mx = std::max(mx, x);
+        min = std::min(min, x);
+    }
+
+    cout << (mx-min)*(n-1) << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
