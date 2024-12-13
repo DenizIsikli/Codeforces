460d0dd37f7f2fb08f481d9236b4c48b740af655
Deniz Isikli <s215818@dtu.dk>
2024-12-14 00:15:13 +0100

    CollectingCoins1294A.cpp

---
+++ Problemsets/800Rating/CollectingCoins1294A.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+using vi = std::vector<int>;
+
+#define int long long
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    std::vector<int> a(3);
+    int n;
+    cin >> a[0] >> a[1] >> a[2] >> n;
+    sort(a.begin(), a.end());
+    n -= 2*a[2]-a[1]-a[0];
+    if (n<0 || n%3!=0) cout << "NO\n";
+    else cout << "YES\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
