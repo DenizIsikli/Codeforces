7c4676599c340ce3bec55301e7fde93b1eff4a5b
Deniz <s215818@dtu.dk>
2024-12-15 23:44:29 +0100

    Codeforces Round 993 (Div. 4)

---
+++ Problemsets/CompleteUpsolves/Codeforces Round 993 (Div. 4)/EasyProblem2044_A.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+using namespace std;
+using vi = std::vector<int>;
+
+#define int long long
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << n-1 << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
