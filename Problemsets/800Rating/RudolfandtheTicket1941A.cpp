ab0d23099aa099200f537012aae431ba061cf8d0
Deniz Isikli <s215818@dtu.dk>
2024-12-20 23:55:40 +0100

    RudolfandtheTicket1941A.cpp

---
+++ Problemsets/800Rating/RudolfandtheTicket1941A.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <bits/stdc++.h>
+
+using namespace std;
+using vi = std::vector<int>;
+
+#define int long long
+
+const int MAX_INT = 1e9;
+const int MIN_INT = -1e9;
+
+template<typename T>
+void read_vector(std::vector<T> &vec) {
+    for (T &val : vec) std::cin >> val;
+}
+
+void solve() {
+
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+
