aa56763c7f67c96d0e695bd3261d1cacd0658218
Deniz <s215818@dtu.dk>
2025-01-21 23:32:59 +0100

    Set2029A.cpp

---
+++ Problemsets/800Rating/Set2029A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int l, r, k;
+    cin >> l >> r >> k;
+
+    cout << max(0ll, (r/k)-l+1) << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
