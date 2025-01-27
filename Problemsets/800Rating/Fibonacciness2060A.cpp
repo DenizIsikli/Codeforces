18900b8cb9c24d73ac8321276afafb77d2c024a7
Deniz <s215818@dtu.dk>
2025-01-28 00:06:54 +0100

    Fibonacciness2060A.cpp

---
+++ Problemsets/800Rating/Fibonacciness2060A.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << 4-set<int>({a+b,c-b,d-c}).size() << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
