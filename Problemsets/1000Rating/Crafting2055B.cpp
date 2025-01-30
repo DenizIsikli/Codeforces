f562a2405451cc3715110e5efae47d4d666db454
Deniz <s215818@dtu.dk>
2025-01-31 00:12:59 +0100

    Crafting2055B.cpp

---
+++ Problemsets/1000Rating/Crafting2055B.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+const int N = 1e6+5;
+int a[N];
+
+void solve() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        a[i] -= x;
+    }
+
+    sort(a, a+n);
+    if (a[0]+a[1]<0) {
+        cout << "NO" << '\n';
+    } else {
+        cout << "YES" << '\n';
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
