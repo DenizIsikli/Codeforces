869a77b4016a3286bb9f01a7c0b79fe8ee980b39
Deniz <s215818@dtu.dk>
2025-02-02 00:49:56 +0100

    KevinandArithmetic2061A.cpp

---
+++ Problemsets/800Rating/KevinandArithmetic2061A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n, x, o, e;
+    cin >> n;
+    for (;n;n--) { cin >> x; (x&1?o:e)++;}
+    cout << (e?o+1:o-1) << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
