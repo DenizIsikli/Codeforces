a348d9787b4dfb3a9225fca12f3eed7f6ae5d194
Deniz <s215818@dtu.dk>
2025-02-05 23:52:30 +0100

    String2062A.cpp

---
+++ Problemsets/800Rating/String2062A.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    int one_cnt = 0;
+    for (char c : s) {
+        if (c == '1') {
+            one_cnt++;
+        }
+    }
+
+    cout << one_cnt << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
