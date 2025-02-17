275839309f20f4a31097609fa0dc8131e9fdfbab
Deniz <s215818@dtu.dk>
2025-02-17 23:15:11 +0100

    SakurrakoandWater2033B.cpp

---
+++ Problemsets/900Rating/SakurrakoandWater2033B.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n;
+    map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int x; cin >> x;
+            mp[i-j] = min(mp[i-j], x);
+        }
+    }
+
+    int res = 0;
+
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        res += -min(0LL, it->second);
+    }
+
+    cout << res << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
