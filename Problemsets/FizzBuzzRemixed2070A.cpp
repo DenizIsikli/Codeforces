b1421a3bd9940412c2843355154da8865998c9ad
Deniz <s215818@dtu.dk>
2025-02-28 21:07:04 +0100

    FizzBuzzRemixed2070A.cpp

---
+++ Problemsets/FizzBuzzRemixed2070A.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << n/15*3+min(3LL, n%15+1) << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
