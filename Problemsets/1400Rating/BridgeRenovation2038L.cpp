2448def1ebc22bef88f9c8501ae74f452ea5668a
Deniz <s215818@dtu.dk>
2025-03-11 00:10:00 +0100

    BridgeRenovation2038L.cpp

---
+++ Problemsets/1400Rating/BridgeRenovation2038L.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    cout << n+(n+5)/6 << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
