b90777c8cc2eeac7cb5d2854a378c8e8c7b34bca
Deniz <s215818@dtu.dk>
2025-03-14 00:33:39 +0100

    EvenPositions1997C.cpp

---
+++ Problemsets/Rating1100/EvenPositions1997C.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int res = n;
+    for (int i = 1; i < n; i += 2) {
+        res += s[i] == '(' ? 1 : -1;
+    }
+
+    cout << res << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
