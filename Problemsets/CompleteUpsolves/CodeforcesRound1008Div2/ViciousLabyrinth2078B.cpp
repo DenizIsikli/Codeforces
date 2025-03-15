d36cd1b81f554579acd96b1dca4d54ec8c5846c0
Deniz <s215818@dtu.dk>
2025-03-15 23:37:02 +0100

    ViciousLabyrinth2078B.cpp

---
+++ Problemsets/CompleteUpsolves/CodeforcesRound1008Div2/ViciousLabyrinth2078B.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k; 
+    for (int i = 1; i <= n-2; i++) {
+        cout << ((k%2) ? n : (n-1)) << " ";
+    }
+    cout << n << " " << n-1 << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
