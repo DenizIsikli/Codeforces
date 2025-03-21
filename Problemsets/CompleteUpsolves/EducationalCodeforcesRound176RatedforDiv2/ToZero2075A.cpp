a928ab04b8546dad70ae3edc0c02d0216315f15b
Deniz <s215818@dtu.dk>
2025-03-22 00:03:57 +0100

    ToZero2075A.cpp

---
+++ Problemsets/CompleteUpsolves/EducationalCodeforcesRound176RatedforDiv2/ToZero2075A.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    int res = 0;
+    if (n%2==1) {
+        n-=k;
+        res = 1;
+    }
+
+    k-=1;
+    res += (n+k-1)/k;
+    cout << res << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
