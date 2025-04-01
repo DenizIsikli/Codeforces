938239ee23da7e5695ae1471345f903dbac3004e
Deniz <s215818@dtu.dk>
2025-04-01 22:32:54 +0200

    TheatreSquare1A.cpp

---
+++ Problemsets/Rating1000/TheatreSquare1A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n, m, a;
+    cin >> n >> m >> a;
+    cout << ((n + a - 1) / a) * ((m + a - 1) / a) << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
+
