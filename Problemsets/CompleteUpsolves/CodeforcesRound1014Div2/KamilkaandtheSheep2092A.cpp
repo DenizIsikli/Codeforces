c2b5a7ca0a015ad6fe6a9fee342b02fa844b03c3
Deniz <s215818@dtu.dk>
2025-04-08 23:47:59 +0200

    KamilkaandtheSheep2092A.cpp

---
+++ Problemsets/CompleteUpsolves/CodeforcesRound1014Div2/KamilkaandtheSheep2092A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    cout << a[n-1]-a[0] << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
