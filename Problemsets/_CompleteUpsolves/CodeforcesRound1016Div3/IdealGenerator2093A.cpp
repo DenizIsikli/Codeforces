6e0256839ef8f88111a9aa2d1415314a37ff5fd0
Deniz <s215818@dtu.dk>
2025-04-11 22:17:11 +0200

    IdealGenerator2093A.cpp

---
+++ Problemsets/_CompleteUpsolves/CodeforcesRound1016Div3/IdealGenerator2093A.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int k;
+    cin >> k;
+    cout << (k%2==0 ? "NO\n" : "YES\n");
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
