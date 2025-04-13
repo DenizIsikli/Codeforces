c082f22ff5753a600dbde5ccf43e127984792e91
Deniz <s215818@dtu.dk>
2025-04-14 01:01:31 +0200

    BobrittoBandito2094B.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1017Div4/BobrittoBandito2094B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n, m, l, r;
+    cin >> n >> m >> l >> r;
+    if (m<=r) cout << 0 << ' ' << m << '\n';
+    else cout << r-m << ' ' << r << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
