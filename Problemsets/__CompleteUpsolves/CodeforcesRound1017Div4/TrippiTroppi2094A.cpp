f9301866adbbbac49a18f56f5e45dfcc10b3b243
Deniz <s215818@dtu.dk>
2025-04-14 01:01:20 +0200

    TrippiTroppi2094A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1017Div4/TrippiTroppi2094A.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    cout << s1[0] << s2[0] << s3[0] << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
