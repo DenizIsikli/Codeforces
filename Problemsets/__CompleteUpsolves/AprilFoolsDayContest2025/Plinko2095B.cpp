455cdcb5e0db25691027224e53f99fc6cd41f766
Deniz <s215818@dtu.dk>
2025-04-23 23:24:11 +0200

    Plinko2095B.cpp

---
+++ Problemsets/__CompleteUpsolves/AprilFoolsDayContest2025/Plinko2095B.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    cout << 17 << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
+
