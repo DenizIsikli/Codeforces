9d062746bc6c44bd0af2e842096870b4d54fc74d
Deniz <s215818@dtu.dk>
2025-04-25 23:24:23 +0200

    StChroma2106B.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1020Div3/StChroma2106B.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    for (int i = 0; i < x; i++) cout << i << " ";
+    for (int i = x+1; i < n; i++) cout << i << " ";
+    if (x<n) cout << x;
+    cout << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
