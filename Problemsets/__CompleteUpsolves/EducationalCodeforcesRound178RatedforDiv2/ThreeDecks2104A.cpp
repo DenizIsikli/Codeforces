73f68c7119d2178fd2ba4a64239d8e787feb3f24
Deniz <s215818@dtu.dk>
2025-04-28 23:46:30 +0200

    ThreeDecks2104A.cpp

---
+++ Problemsets/__CompleteUpsolves/EducationalCodeforcesRound178RatedforDiv2/ThreeDecks2104A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ((a+b+c)%3==0&&b<=(a+b+c)/3) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
