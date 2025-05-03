79bdf451861bc5108eb35c6b52337236df9f6eb9
Deniz <s215818@dtu.dk>
2025-05-03 23:45:38 +0200

    PermutationWarmUp2108A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1022Div2/PermutationWarmUp2108A.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    int k = n/2;
+    if (n%2) { cout << k*(k+1)+1 << '\n'; return; }
+    cout << k*k+1 << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
