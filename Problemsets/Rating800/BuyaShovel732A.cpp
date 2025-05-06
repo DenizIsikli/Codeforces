84b12d01025b5a75729764c69f3084a1ad58b46d
Deniz Isikli <s215818@dtu.dk>
2025-05-06 23:41:14 +0200

    BuyaShovel732A.cpp

---
+++ Problemsets/Rating800/BuyaShovel732A.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int k, r;
+    cin >> k >> r;
+    int x = 1;
+    while ((k*x)%10 != 0 && (k*x)%10 != r) {
+        x++;
+    }
+    cout << x << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
+
