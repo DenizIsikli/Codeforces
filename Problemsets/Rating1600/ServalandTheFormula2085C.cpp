d93e0eef258e4a2a5f2c0378dca58b87e54199b5
Deniz Isikli <s215818@dtu.dk>
2025-05-09 23:51:19 +0200

    ServalandTheFormula2085C.cpp

---
+++ Problemsets/Rating1600/ServalandTheFormula2085C.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    if(x==y) {
+        cout << -1 << "\n";
+        return;
+    }
+    cout << (1<<30)-max(x,y) << "\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
