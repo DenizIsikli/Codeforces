4d321d398ccc522a4bf6a5eda37e5aacf8fb97e5
Deniz Isikli <s215818@dtu.dk>
2025-05-13 23:58:14 +0200

    LRCandVIP2107A.cpp

---
+++ Problemsets/Rating800/LRCandVIP2107A.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int mn = *min_element(a.begin(), a.end());
+    int mx = *max_element(a.begin(), a.end());
+    if (mn == mx){
+        cout << "No\n";
+        return;
+    }
+    cout << "Yes\n";
+    for (int i = 0; i < n; i++){
+        cout << (1 + (a[i] == mx)) << " \n"[i + 1 == n];
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
