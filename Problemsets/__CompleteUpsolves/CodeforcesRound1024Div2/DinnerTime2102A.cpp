b03895d367c9cc3739309765a4bcfaf95de790fa
Deniz Isikli <s215818@dtu.dk>
2025-05-14 23:29:57 +0200

    DinnerTime2102A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1024Div2/DinnerTime2102A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n, m, p, q;
+    cin >> n >> m >> p >> q;
+    if (n%p==0 && (n/p)*q!=m) cout << "NO\n";
+    else cout << "YES\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
