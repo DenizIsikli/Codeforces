4a57e48a10c8e48143c3893ac45237a5998da6dd
Deniz Isikli <s215818@dtu.dk>
2025-05-16 23:51:27 +0200

    TurtleandGoodPairs2003C.cpp

---
+++ Problemsets/Rating1200/TurtleandGoodPairs2003C.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < n/2; i++) {
+        cout << s[i] << s[n-i-1];
+    }
+
+    if (n&1) cout << s[n/2];
+    cout << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
