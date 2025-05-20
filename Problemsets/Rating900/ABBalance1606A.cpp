694e69d1841ffdacb908f7944bb33d4ca73c228a
Deniz Isikli <s215818@dtu.dk>
2025-05-20 23:58:58 +0200

    ABBalance1606A.cpp

---
+++ Problemsets/Rating900/ABBalance1606A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    s[s.size()-1]=s[0];
+    cout << s << "\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
