62282a68d7424ef5e2cd86314c10c7f72006fa7b
Deniz Isikli <s215818@dtu.dk>
2025-05-22 23:24:30 +0200

    ReverseCardEasyVersion1967B1.cpp

---
+++ Problemsets/Rating1400/ReverseCardEasyVersion1967B1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    int r = n;
+    for (int b = 2; b <= m; b++) {
+        r += (n/b+1)/b;
+    }
+
+    cout << r << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
