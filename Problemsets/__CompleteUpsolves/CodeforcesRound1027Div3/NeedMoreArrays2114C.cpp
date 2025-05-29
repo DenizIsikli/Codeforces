0b8a2a62eb25482d554c36e182b1d114fa5cf8a1
Deniz Isikli <s215818@dtu.dk>
2025-05-29 22:21:03 +0200

    NeedMoreArrays2114C.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1027Div3/NeedMoreArrays2114C.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int last = -1, res = 0;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        if (x-last > 1) {
+            res++;
+            last = x;
+        }
+    }
+    cout << res << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
