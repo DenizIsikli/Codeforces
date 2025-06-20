ad60e8bd6992c55ad2ff368074b637aa00fff597
Deniz Isikli <s215818@dtu.dk>
2025-06-20 23:57:12 +0200

    TreasureHunt2090A.cpp

---
+++ Problemsets/Rating800/TreasureHunt2090A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int x,y,a;
+    cin>>x>>y>>a;
+    if(a%(x+y)<x){
+        cout<<"NO"<<endl;
+    }else{
+        cout<<"YES"<<endl;
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
