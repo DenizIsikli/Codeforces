6e6b117275458792b2925d4e0273d8ae3e1283d0
Deniz Isikli <s215818@dtu.dk>
2025-06-23 23:57:18 +0200

    SquarePool2120B.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1033Div2andCodeNite2025/SquarePool2120B.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n,s,dx,dy,x,y;
+    cin>>n>>s;
+    int ans=0;
+    for (int i=0;i<n;i++){
+        cin>>dx>>dy>>x>>y;
+        if(dx==dy)ans+=(x==y);
+        else ans+=(x+y==s);
+    }
+    cout<<ans<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
