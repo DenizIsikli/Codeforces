d94d9863bd567449ea8db08c26572eba34b7329e
Deniz Isikli <s215818@dtu.dk>
2025-07-05 23:48:24 +0200

    ComeTogether1845B.cpp

---
+++ Problemsets/Rating900/ComeTogether1845B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int a,b,c,d,e,f;
+    cin>>a>>b>>c>>d>>e>>f;
+    cout<<(abs(a-c)+abs(b-d)+1+abs(a-e)+abs(b-f)+1-abs(c-e)-abs(d-f))/2<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
