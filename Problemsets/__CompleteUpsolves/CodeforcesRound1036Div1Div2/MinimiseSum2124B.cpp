b3c0f0eed4391db81e495788bb535efe3b863cd4
Deniz Isikli <s215818@dtu.dk>
2025-07-13 22:28:49 +0200

    MinimiseSum2124B.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1036Div1Div2/MinimiseSum2124B.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(auto&x:a)cin>>x;
+    cout<<min(2*a[0],a[0]+a[1])<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
