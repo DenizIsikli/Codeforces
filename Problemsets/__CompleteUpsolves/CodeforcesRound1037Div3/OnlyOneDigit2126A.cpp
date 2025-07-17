06e74f1905e9f95036eb8d0ff587dc1c5af1eda3
Deniz Isikli <s215818@dtu.dk>
2025-07-17 23:21:52 +0200

    OnlyOneDigit2126A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1037Div3/OnlyOneDigit2126A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin>>n;
+    int ans=n;
+    while(n){
+        ans=min(ans,n%10);
+        n/=10;
+    }
+    cout<<ans<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
