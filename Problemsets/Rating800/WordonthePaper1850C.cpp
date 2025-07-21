9374c85285942c6629762773eefb81b816907fcc
Deniz Isikli <s215818@dtu.dk>
2025-07-21 23:47:09 +0200

    WordonthePaper1850C.cpp

---
+++ Problemsets/Rating800/WordonthePaper1850C.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    for (int r = 0; r < 8; r++) {
+		for (int c = 0; c < 8; c++) {
+			char x;
+			cin >> x;
+			if (x != '.') {cout << x;}
+		}
+	}	
+	cout << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
