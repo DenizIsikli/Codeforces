12d52db44b768d51af089200c11c37ba6a19af1d
Deniz Isikli <s215818@dtu.dk>
2025-08-04 23:53:38 +0200

    DistanceandAxis1401A.cpp

---
+++ Problemsets/Rating900/DistanceandAxis1401A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n,k;
+    cin>>n>>k;
+    if(n<k)cout<<k-n<<endl;
+    else if(n%2==k%2)cout<<0<<endl;
+    else cout<<1<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
