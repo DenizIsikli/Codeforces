f5e8fcdc284e42f9931c2ede7090b63d9b7efb88
Deniz Isikli <s215818@dtu.dk>
2025-08-14 23:41:04 +0200

    AlternatingSeries2131B.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1042Div3/AlternatingSeries2131B.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i%2)cout<<"-1 ";
+        else if(i<n)cout<<"3 ";
+        else cout<<"2 ";
+    }
+    cout<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
