35cff4617f99dab2fc1badd1c4294dfd2b36bbbd
Deniz Isikli <s215818@dtu.dk>
2025-08-20 23:58:32 +0200

    AntiLightsCellGuessing1610A.cpp

---
+++ Problemsets/Rating900/AntiLightsCellGuessing1610A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n,m;
+    cin>>n>>m;
+    if(n==1&&m==1)cout<<"0"<<endl;
+    else if(min(n,m)==1)cout<<"1"<<endl;
+    else cout<<"2"<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
