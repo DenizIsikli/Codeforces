044ef6e045dbfb66c6076f0039cbf13872ea677f
Deniz Isikli <s215818@dtu.dk>
2025-08-29 00:07:38 +0200

    Redstone2133A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1044Div2/Redstone2133A.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    set<int>s;
+    for(auto&x:a)cin>>x;
+    for(auto&x:a){
+        if(s.count(x)){
+            cout<<"YES"<<endl;
+            return;
+        }
+        s.insert(x);
+    }
+    cout<<"NO"<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
