e2da62d8c13dba13c8b09df9deece976daa686a9
Deniz Isikli <s215818@dtu.dk>
2025-08-29 19:09:39 +0200

    Villagers2133B.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1044Div2/Villagers2133B.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(auto&x:a)cin>>x;
+    sort(a.begin(),a.end());
+    int ans=0;
+    for(int i=n-1;i>=0;i-=2){ans+=a[i];}
+    cout<<ans<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
