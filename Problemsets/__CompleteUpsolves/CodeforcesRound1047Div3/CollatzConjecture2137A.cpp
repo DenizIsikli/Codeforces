c992d3483824bcd3a03a7db06e42e34c0da20c7c
Deniz Isikli <s215818@dtu.dk>
2025-09-07 21:44:12 +0200

    CollatzConjecture2137A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1047Div3/CollatzConjecture2137A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+int k,x;
+
+void solve() {
+    cin>>k>>x;
+    cout<<(x<<k)<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
