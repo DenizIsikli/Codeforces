db990c960669710b5f6ee936967b99b9815fa44f
Deniz Isikli <s215818@dtu.dk>
2025-09-17 23:29:03 +0200

    SublimeSequence2148A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1050Div4/SublimeSequence2148A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int x,n;
+    cin>>x>>n;
+    if(n&1){cout<<x<<endl;}
+    else{cout<<0<<endl;}
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
