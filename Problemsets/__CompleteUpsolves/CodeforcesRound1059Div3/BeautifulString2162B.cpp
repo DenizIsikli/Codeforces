589bff35ab3a022ca3c47a1308f477abbfebfb1d
Deniz Isikli <s215818@dtu.dk>
2025-10-18 23:55:53 +0200

    BeautifulString2162B.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1059Div3/BeautifulString2162B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
