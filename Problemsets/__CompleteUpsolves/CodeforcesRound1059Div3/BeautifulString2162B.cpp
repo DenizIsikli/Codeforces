ae07a0f35d60c6cbb2cdd3b00a228c0ee1bf828d
Deniz Isikli <s215818@dtu.dk>
2025-10-19 23:40:42 +0200

    BeautifulString2162B.cpp

--- Problemsets/__CompleteUpsolves/CodeforcesRound1059Div3/BeautifulString2162B.cpp
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1059Div3/BeautifulString2162B.cpp
@@ -6,7 +6,15 @@
 using namespace std;
 
 void solve() {
-
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    vector<int>pos;
+    for(int i=0;i<n; i++)if(s[i]=='0')pos.push_back(i+1);
+    cout<<(int)pos.size()<<'\n';
+    for(auto&z:pos)cout<<z<<' ';
+    cout<<'\n';
 }
 
 signed main() {
