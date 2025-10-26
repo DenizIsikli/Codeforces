088a198947fa3db805011039f708d733c7aa3f08
Deniz Isikli <s215818@dtu.dk>
2025-10-26 22:01:45 +0100

    Template refactor

--- template/templateSTD.cpp
+++ template/templateSTD.cpp
@@ -1,24 +1,19 @@
 #include <bits/stdc++.h>
-
 #define int long long
 #define endl '\n'
-
 using namespace std;
 
-void solve() {
+void solve(){
 
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int t;
-    std::cin >> t;
-
-    while (t--) {
+    std::cin>>t;
+    while(t--){
         solve();
     }
-
     return 0;
 }
--- template/templateSTDOCF.cpp
+++ template/templateSTDOCF.cpp
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
-
 #define int long long
 #define endl '\n'
-
 using namespace std;
 
-void solve() {
+void solve(){
 
 }
 
-signed main() {
+signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     solve();
-
     return 0;
 }
