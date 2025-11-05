8ad332a7f3a34584bc0d8cfa1740fd66d2413cac
Deniz Isikli <s215818@dtu.dk>
2025-11-05 23:58:55 +0100

    KevinandCombinationLock2048A.cpp

---
+++ Problemsets/Rating800/KevinandCombinationLock2048A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    cout<<(n%33?"NO\n":"YES\n");
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    std::cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
