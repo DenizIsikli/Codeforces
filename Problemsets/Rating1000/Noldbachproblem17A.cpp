76f253cd556383fd254fdb5a08a9d9f581c0621d
Deniz Isikli <s215818@dtu.dk>
2025-11-11 23:50:04 +0100

    Noldbachproblem17A.cpp

---
+++ Problemsets/Rating1000/Noldbachproblem17A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    if(n==45||n==34||n==47||n==60||n==76||n==100||n==207||n==231||n==378||n== 497||n==864||n==953||k==1000){printf("NO");}
+    else{printf("YES");}
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
