df10a121ccd01a6c5f1c3e752a82f0ef7e06634d
Deniz Isikli <s215818@dtu.dk>
2025-11-16 23:59:44 +0100

    WonderfulSticks2096A.cpp

---
+++ Problemsets/Rating800/WonderfulSticks2096A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int l=1,r=n;
+    vector<int>a(n);
+    for(int i=n-2;i>=0;i--){
+        if(s[i]=='<'){
+            a[i+1]=l;
+            l++;
+        }
+        if(s[i]=='>'){
+            a[i]=r;
+            r--;
+        }
+    }
+    a[0]=l;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    std::cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
