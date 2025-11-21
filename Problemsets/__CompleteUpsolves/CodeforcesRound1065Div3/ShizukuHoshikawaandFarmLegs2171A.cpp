d94d950a610e85af83fc08039fbca18deaf6b6e8
Deniz Isikli <s215818@dtu.dk>
2025-11-21 23:36:10 +0100

    ShizukuHoshikawaandFarmLegs2171A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1065Div3/ShizukuHoshikawaandFarmLegs2171A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    cout<<(n&1?0:n/4+1)<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
