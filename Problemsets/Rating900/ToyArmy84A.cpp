2a267efeccf1bb5a530fbc2cad5d8a3d944abdd1
Deniz Isikli <s215818@dtu.dk>
2025-12-18 23:08:51 +0100

    ToyArmy84A.cpp

---
+++ Problemsets/Rating900/ToyArmy84A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    cout<<3*n/2;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
