cf2f3f279a83b4572230c2f67662900f3c2bc33f
Deniz Isikli <s215818@dtu.dk>
2026-01-10 23:58:01 +0100

    NeedleinaHaystack2174A.cpp

---
+++ Problemsets/Rating1200/NeedleinaHaystack2174A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#define int long long
+
+#define endl '\n'
+using namespace std;
+
+int c[26];
+
+void solve(){
+    for(int i=0;i<26;i++)c[i]=0;
+    string s1,s2;
+    cin>>s1>>s2;
+    for(int i=0;i<s2.size();i++)c[s2[i]-'a']++;
+    for(int i=0;i<s1.size();i++)c[s1[i]-'a']--;
+    for(int i=0;i<26;i++)if(c[i]<0){cout<<"Impossible\n";return;}
+    int idx=0;
+    for(int i=0;i<26;i++){
+        while(idx<s1.size()&&s1[idx]-'a'<=i){cout<<s1[idx];idx++;}
+        for(int j=0;j<c[i];j++)cout<<(char)(i+'a');
+    }
+    cout<<"\n";
+}
+
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+    return 0;
+}
