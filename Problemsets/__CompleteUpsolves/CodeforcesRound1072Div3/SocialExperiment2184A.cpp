f44dffcbbbdafe93f24ff1d1fdbb028d300e3bd1
Deniz Isikli <s215818@dtu.dk>
2026-01-13 21:03:02 +0100

    SocialExperiment2184A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1072Div3/SocialExperiment2184A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n==2)cout<<2<<endl;
+    if(n==3)cout<<3<<endl;
+    if(n>=4)cout<<n%2<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
