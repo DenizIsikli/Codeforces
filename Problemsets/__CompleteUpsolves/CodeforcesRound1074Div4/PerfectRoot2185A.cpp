688376acc6587574340067f676a80a7be5e958d6
Deniz Isikli <s215818@dtu.dk>
2026-01-18 23:17:31 +0100

    PerfectRoot2185A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1074Div4/PerfectRoot2185A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    while(n){cout<<n--<<" ";}
+    cout<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
