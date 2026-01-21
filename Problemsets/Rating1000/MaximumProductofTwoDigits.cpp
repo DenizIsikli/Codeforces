9e439ad709f935446afdc41aabc176dd0bb443ec
Deniz Isikli <s215818@dtu.dk>
2026-01-21 23:06:00 +0100

    MaximumProductofTwoDigits.cpp

---
+++ Problemsets/Rating1000/MaximumProductofTwoDigits.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProduct(int n) {
+        string s=to_string(n);
+        sort(s.begin(),s.end());
+        return (s[s.size()-1]-'0')*(s[s.size()-2]-'0');
+    }
+};
