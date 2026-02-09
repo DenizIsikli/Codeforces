04263c450ee0c96adf46bb22df9f154b1960d0ea
Deniz Isikli <s215818@dtu.dk>
2026-02-09 23:58:58 +0100

    LawnMower2194A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1078Div2/LawnMower2194A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+void solve(){
+    int n,w;
+    cin>>n>>w;
+    cout<<n-(n/w)<<endl;
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;cin>>t;
+    while(t--){solve();}
+    return 0;
+}
