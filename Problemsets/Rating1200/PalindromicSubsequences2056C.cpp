32573b0ff674ca35b2ea13f247ce724758d4462c
Deniz Isikli <s215818@dtu.dk>
2026-02-18 23:33:43 +0100

    PalindromicSubsequences2056C.cpp

---
+++ Problemsets/Rating1200/PalindromicSubsequences2056C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    cout<<"1 ";for(int i=0;i<n-3;i++)cout<<i+2<<" ";cout<<"1 1"<<endl;
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;cin>>t;
+    while(t--){solve();}
+    return 0;
+}
