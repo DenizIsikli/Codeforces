d7bf71977892c44b12de0d75b6cf2665a6c7f2d5
Deniz Isikli <s215818@dtu.dk>
2026-03-02 00:02:49 +0100

    TestGenerator2203C.cpp

--- Problemsets/__CompleteUpsolves/misc/EducationalCodeforcesRound187Div2/TestGenerator2203C.cpp
+++ Problemsets/__CompleteUpsolves/misc/EducationalCodeforcesRound187Div2/TestGenerator2203C.cpp
@@ -7,8 +7,18 @@ signed main(){
     cin.tie(nullptr);
     int t;cin>>t;
     while(t--){
-
+        int s,m;cin>>s>>m;
+        if((s&-s)<(m&-m)){
+            cout<<-1<<endl;
+            continue;
+        }
+        int ans=0;
+        for(int i=1;i<=61;i++){
+            int msk=(1LL<<i)-1;
+            int s_=s&msk,m_=m&msk;
+            if(m_)ans=max(ans,(s_+m_-1)/m_);
+        }
+        cout<<ans<<endl;
     }
     return 0;
 }
-
