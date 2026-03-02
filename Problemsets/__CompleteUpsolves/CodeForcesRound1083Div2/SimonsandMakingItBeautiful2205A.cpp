187b8f2109df00433b22544dbdf1185b51fc19b6
Deniz Isikli <s215818@dtu.dk>
2026-03-03 00:01:04 +0100

    SimonsandMakingItBeautiful2205A.cpp fix

--- Problemsets/__CompleteUpsolves/CodeForcesRound1083Div2/SimonsandMakingItBeautiful2205A.cpp
+++ Problemsets/__CompleteUpsolves/CodeForcesRound1083Div2/SimonsandMakingItBeautiful2205A.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
+#define int long long
 #define endl '\n'
 using namespace std;
-int a[505];
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;cin>>t;
     while(t--){
         int n;cin>>n;
+        vector<int>a(n);
         for(int i=0;i<n;i++){
             cin>>a[i];
-            if(a[i]==n)swap(a[1],a[i]);
+            if(a[i]==n)swap(a[0],a[i]);
         }
-        for(int i = 1; i <= n; i++) printf("%d ", a[i]); puts("");
+        for(int i=0;i<n;i++)cout<<a[i]<<" ";
+        cout<<endl;
     }
     return 0;
 }
