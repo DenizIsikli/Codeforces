15863d395fdbdbf7fa1cb635957ff73e713f4a40
Deniz Isikli <s215818@dtu.dk>
2026-03-07 23:18:51 +0100

    templateSTD.cpp

--- template/templateSTD.cpp
+++ template/templateSTD.cpp
@@ -2,12 +2,15 @@
 #define int long long
 #define endl '\n'
 using namespace std;
+void solve(){
+
+}
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;cin>>t;
     while(t--){
-
+        solve();
     }
     return 0;
 }
