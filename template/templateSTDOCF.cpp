dfb6f20836719c9541bad21270dd1bdaf9f6d8f6
Deniz Isikli <s215818@dtu.dk>
2026-03-07 23:19:02 +0100

    templateSTDOCF.cpp

--- template/templateSTDOCF.cpp
+++ template/templateSTDOCF.cpp
@@ -2,9 +2,12 @@
 #define int long long
 #define endl '\n'
 using namespace std;
+void solve(){
+
+}
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    solve();
     return 0;
 }
