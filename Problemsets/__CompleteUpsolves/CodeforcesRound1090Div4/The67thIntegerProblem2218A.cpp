5ae0d79606d0137f46df47d2b071efb644a76d89
Deniz Isikli <s215818@dtu.dk>
2026-04-09 00:34:11 +0200

    The67thIntegerProblem2218A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1090Div4/The67thIntegerProblem2218A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+void solve(){
+    int x;
+    cin>>x;
+    cout<<x+1<<endl;
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
