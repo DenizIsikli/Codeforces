bdd57aa9462935adb92f4f0bd2594b39fca712e6
Deniz Isikli <s215818@dtu.dk>
2026-04-20 20:35:44 +0200

    Minor fix

--- template/templateSTD.cpp
+++ template/templateSTD.cpp
@@ -2,16 +2,13 @@
 #define int long long
 #define endl '\n'
 using namespace std;
-
 void solve(){
 
 }
-
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin>>t;
+    int t;cin>>t;
     while(t--){
         solve();
     }
