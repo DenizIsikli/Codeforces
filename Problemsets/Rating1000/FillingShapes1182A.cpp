a55a49822302bc64b2b02813ff1f468981d5f97e
Deniz Isikli <s215818@dtu.dk>
2026-04-26 23:57:20 +0200

    FillingShapes1182A.cpp

---
+++ Problemsets/Rating1000/FillingShapes1182A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    cout<<(n%2?0:1<<n/2);
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
