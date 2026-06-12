b5b655702ea5c1ef0899c99d53a9037849d09a81
Deniz Isikli <s215818@dtu.dk>
2026-06-12 23:53:01 +0200

    GamesontheTrain2236A.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1103Div3/GamesontheTrain2236A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+void solve(){
+    int n;cin>>n;
+    vector<int>a(n);
+    for(auto&x:a)cin>>x;
+    sort(a.begin(),a.end());
+    cout<<a[n-1]-a[0]+1<<endl;
+}
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
