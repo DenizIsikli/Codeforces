be9dca97c7fa2f97a63f18d471362228ac8472f7
Deniz Isikli <s215818@dtu.dk>
2026-07-13 22:45:48 +0200

    ezraftandArray2246B.cpp

---
+++ Problemsets/__CompleteUpsolves/CodeforcesRound1108Div2/ezraftandArray2246B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    if(n==1){
+        cout<<1<<endl;
+        return;
+    }
+    if(n==2){
+        cout<<-1<<endl;
+        return;
+    }
+    int x=3;
+    cout<<"1 2 ";
+    for(int i=3;i<=n;i++){
+        cout<<x<<" ";
+        x*=2;
+    }
+    cout<<endl;
+}
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
